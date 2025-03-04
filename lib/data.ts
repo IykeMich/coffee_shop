656ef0869dd8b6ebc95e0ace02366ce82f1913ef
IykeMich <iykemich361@gmail.com>
2025-03-04 01:51:14 +0100

    worked on image path 2

--- lib/data.ts
+++ lib/data.ts
@@ -74,21 +74,21 @@ const data: Data = {
         }
     ],
     partners: [
-        {name: "india GQ", url: "/images/partner-1.svg"},
-        {name: "india GR", url: "/images/partner-2.svg"},
-        {name: "india GS", url: "/images/partner-3.svg"},
-        {name: "india GT", url: "/images/partner-4.svg"},
-        {name: "india GU", url: "/images/partner-5.svg"}
+        {name: "india GQ", url: "/partner-1.svg"},
+        {name: "india GR", url: "/partner-2.svg"},
+        {name: "india GS", url: "/partner-3.svg"},
+        {name: "india GT", url: "/partner-4.svg"},
+        {name: "india GU", url: "/partner-5.svg"}
     ],
     products: [
-        {id: 1, name: "Baarbara Estate - Pineapple", price: 525, size: 250, imageUrl: '/images/product-1.svg'},
-        {id: 2, name: "Cold Brew Blid", price: 445, size: 250, imageUrl: '/images/product-2.svg'},
-        {id: 3, name: "Expresso Milk Blend", price: 445, size: 250, imageUrl:  '/images/product-2.svg'},
-        {id: 4, name: "Mandalkhan Estate", price: 445, size: 250, imageUrl: '/images/product-3.svg'},
-        {id: 5, name: "Mandalkhan Estate", price: 445, size: 250, imageUrl: '/images/product-4.svg'},
-        {id: 6, name: "Mandalkhan Estate Insta Pour", price: 345, size: 250, imageUrl: '/images/product-3.svg'},
-        {id: 7, name: "Monsoon Malabar", price: 470, size: 250, imageUrl: '/images/product-4.svg'},
-        {id: 8, name: "Monsoon Malabar", price: 470, size: 250, imageUrl: '/images/product-4.svg'},
+        {id: 1, name: "Baarbara Estate - Pineapple", price: 525, size: 250, imageUrl: '/product-1.svg'},
+        {id: 2, name: "Cold Brew Blid", price: 445, size: 250, imageUrl: '/product-2.svg'},
+        {id: 3, name: "Expresso Milk Blend", price: 445, size: 250, imageUrl:  '/product-2.svg'},
+        {id: 4, name: "Mandalkhan Estate", price: 445, size: 250, imageUrl: '/product-3.svg'},
+        {id: 5, name: "Mandalkhan Estate", price: 445, size: 250, imageUrl: '/product-4.svg'},
+        {id: 6, name: "Mandalkhan Estate Insta Pour", price: 345, size: 250, imageUrl: '/product-3.svg'},
+        {id: 7, name: "Monsoon Malabar", price: 470, size: 250, imageUrl: '/product-4.svg'},
+        {id: 8, name: "Monsoon Malabar", price: 470, size: 250, imageUrl: '/product-4.svg'},
     ],
     benefits: [
         {id: 1, image:"/images/benefits/benefit-1.svg", title: "speciality coffee", subtitle: "Speciality grade coffees that’s rated above 80+ on taste"},
