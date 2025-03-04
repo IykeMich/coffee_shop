582b3709e0744800bfda9f80f8b3f28241ba3672
IykeMich <iykemich361@gmail.com>
2025-03-04 02:14:31 +0100

    Image wahala

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
   images: {
-    domains: [
-      'https://coffee-shop-psi-amber.vercel.app/',
-    ]
+    remotePatterns: [
+        {
+            protocol: "https",
+            hostname: "coffee-shop-psi-amber.vercel.app",
+        },
+    ],
   }
 };
 
