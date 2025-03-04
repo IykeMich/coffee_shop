c96c9fd6beef1aaafbee35154188c0c2de66c73f
IykeMich <iykemich361@gmail.com>
2025-03-04 02:05:21 +0100

    worked on image path 4

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      'https://coffee-shop-psi-amber.vercel.app/',
+    ]
+  }
 };
 
 export default nextConfig;
